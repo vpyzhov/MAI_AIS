6a3ae30407621920602dc0eebc756a8754b64adc
VPyzhov <pyzhov.vasily@gmail.com>
2023-10-03 11:32:20 +0300

    aa-a

---
+++ ProjectCPP/build/CMakeFiles/hellowWorld.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hellowWorld.dir/VPTestCPP.cpp.o"
+  "CMakeFiles/hellowWorld.dir/VPTestCPP.cpp.o.d"
+  "hellowWorld"
+  "hellowWorld.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hellowWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
